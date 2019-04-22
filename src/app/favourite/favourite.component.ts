59cd885c9ad240699d9287bca1be4305f8bfd737
ShaneF67 <shanefeehily9@hotmail.com>
2019-04-22 16:21:25 -0500

    Changing starFilled field in favourite component

--- src/app/favourite/favourite.component.ts
+++ src/app/favourite/favourite.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'favourite',
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FavouriteComponent implements OnInit {
 
-  starFilled = false;
+  @Input() starFilled: boolean;
   constructor() { }
 
   ngOnInit() {
